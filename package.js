95243025882f8f56a277dd7b9cee58ac5f09936f
ecarlotti <ecarlotti@hotmail.com>
2020-07-30 16:42:05 -0300

    removed version lock from npm packages

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ecarlotti:collection-table',
-  version: '0.5.0',
+  version: '0.5.1',
   summary: 'A CRUD table template for displaying collection items in Meteor.',
   git: 'https://github.com/ecarlotti/meteor-collection-table.git'
 });
@@ -13,15 +13,20 @@ Package.on_use(function(api) {
     'underscore',
     'jquery',
     'less',
-    'aramk:jquery-doubletap@0.1.0',
-    'matb33:collection-hooks@0.8.0',
-    'reactive-var@1.0.3',
-    'urbanetic:utility@1.2.0'
+    'aramk:jquery-doubletap',            // @0.1.0
+    'matb33:collection-hooks',           // @0.8.0
+    'reactive-var',                      // @1.0.3
+    'urbanetic:utility'                  // @1.2.0
   ], 'client');
-  api.use(['iron:router@1.0.13'], 'client', {weak: true});
+  
   api.use([
-    'aslagle:reactive-table@0.8.1'
+    'iron:router'                        // @1.0.13
+  ], 'client', {weak: true});
+  
+  api.use([
+    'aslagle:reactive-table'             // @0.8.1
   ], ['client', 'server']);
+  
   api.imply(['aslagle:reactive-table'], ['client', 'server']);
   api.addFiles([
     'src/collectionTable.html',
